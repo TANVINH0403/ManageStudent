078d69bafdb7eaae1500ea1f0ffff043393946d4
thanhhai375 <thanhhai30072005@gmail.com>
2026-04-12 02:00:29 +0700

    Update: UI Dashboard

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './assets/global.css' // <-- Chú ý sửa dòng này nhé
+// src/main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './App.css'; // Hoặc đường dẫn đến file css toàn cục của bạn
+import { Provider } from 'react-redux';
+import { store } from './redux/store.js'; // Nhập "Bộ não" vừa tạo
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* Bọc toàn bộ App bằng Provider để truyền dữ liệu đi mọi nơi */}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
